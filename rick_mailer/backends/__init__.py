6430346e00508fb286eb112da8be0f5afab958fe
jpinheiro <jpinheiro@blackshield.pt>
2023-05-31 16:07:45 +0100

    removal of optional(); version bump

--- rick_mailer/backends/__init__.py
+++ rick_mailer/backends/__init__.py
@@ -1,5 +1,3 @@
-from rick.util.misc import optional
-
 from .base import BaseEmailBackend, registry
 from .console import ConsoleEmailBackend
 from .smtp import SMTPEmailBackend
@@ -9,14 +7,14 @@ from .locmem import MemEmailBackend
 def SMTPFactory(cfg: dict, fail_silently=False) -> SMTPEmailBackend:
     cls = registry.get("smtp")
     return cls(
-        host=optional("smtp_host", cfg, "localhost"),
-        port=optional("smtp_port", cfg, 25),
-        username=optional("smtp_username", cfg, ""),
-        password=optional("smtp_password", cfg, ""),
-        use_tls=optional("smtp_use_tls", cfg, False),
+        host=cfg.get("smtp_host", "localhost"),
+        port=cfg.get("smtp_port", 25),
+        username=cfg.get("smtp_username", ""),
+        password=cfg.get("smtp_password", ""),
+        use_tls=cfg.get("smtp_use_tls", False),
         fail_silently=fail_silently,
-        use_ssl=optional("smtp_use_ssl", cfg, False),
-        timeout=optional("smtp_timeout", cfg, None),
-        ssl_keyfile=optional("smtp_ssl_keyfile", cfg),
-        ssl_certfile=optional("smtp_ssl_certfile", cfg),
+        use_ssl=cfg.get("smtp_use_ssl", False),
+        timeout=cfg.get("smtp_timeout", None),
+        ssl_keyfile=cfg.get("smtp_ssl_keyfile"),
+        ssl_certfile=cfg.get("smtp_ssl_certfile"),
     )
